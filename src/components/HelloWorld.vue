20a33f6ae4b6658672081978a1f92c3f4d0f33dc
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2018-01-08 12:40:49 +0100

    standard directives
    standard directives

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Nazioni</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li v-for="country in countries">{{ country.name }}</li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div>
+      <input type="text" v-model="text">
+    </div>
+    <b>two-way data binding:</b> <span v-text="text"></span>
+    <button v-on:click="clearMessage">Pulisci testo</button>
   </div>
 </template>
 
@@ -25,7 +18,21 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      countries: [
+        {name: 'Italia'},
+        {name: 'Inghilterra'},
+        {name: 'Spagna'}
+      ],
+      text: ''
+    }
+  },
+  methods: {
+    clearMessage: function () {
+      if (this.text != '')
+        this.text = '';
+      else
+        alert("nessun testo da cancellare");
     }
   }
 }
